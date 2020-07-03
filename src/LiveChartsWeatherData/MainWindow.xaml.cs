e8dc050e5684138d1e57a3cc87ef280f9929b72e
Milad Vafaeifard <milad_vafaeifard@rcomext.com>
2020-07-03 15:05:52 +0200

    init files

---
+++ src/LiveChartsWeatherData/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
+using System.IO;
+using System.Windows;
+using Weather;
+
+namespace LiveChartsWeatherData
+{
+    public partial class MainWindow : Window
+    {
+        private static readonly string filename = @"..\..\..\..\pond_data\Environmental_Data_Deep_Moor_2012.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Add LineSeries
+            var series = new LineSeries()
+            {
+                Title = "Barometric Pressure",
+                // ChartValues of WeatherObservation
+                Values = LoadData(new ChartValues<WeatherObservation>())
+            };
+
+            // Mapping Functions from raw values to doubles for Axis (X & Y)
+            var woXY = Mappers.Xy<WeatherObservation>();
+            woXY.X(wo => wo.TimeStamp.Ticks);
+            woXY.Y(wo => wo.Barometric_Pressure);
+
+            // Series Collection containing the LineSeries
+            MySeriesCollection = new SeriesCollection(woXY)
+            {
+                series
+            };
+
+            DataContext = this; // for databinding
+        }
+
+        private static ChartValues<WeatherObservation> LoadData(ChartValues<WeatherObservation> values)
+        {
+            var start = DateTime.Parse("2012-01-02 00:00:00");
+            var end = DateTime.Parse("2012-01-02 17:00:00");
+
+            using (var text = new StreamReader(filename))
+            {
+                text.ReadLine();
+
+                var woValues = WeatherData.ReadRange(text, start, end);
+
+                values.AddRange(woValues);
+                return values;
+            }
+        }
+
+        public SeriesCollection MySeriesCollection { get; set; }
+
+        public Func<double, string> X_Label_Formatter => (d) => (new DateTime((long)d)).ToString("yyyy-MM:dd HH:mm:ss");
+        public Func<double, string> Y_Label_Formatter => (d) => d.ToString("###.00");
+    }
+}
---
+++ src/WeatherData/WeatherObservation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Weather
+{
+    public struct WeatherObservation
+    {
+        public DateTime TimeStamp;
+        public float Barometric_Pressure;
+
+        public static WeatherObservation Parse(string text)
+        {
+            var data = text.Split('\t');
+
+            Debug.Assert(data.Length == 8);
+
+            var timestamp = DateTime.Parse(data[(int)WeatherObservationMetrics.Date_Time].Replace("_", "-"));
+            var pressure = float.Parse(data[(int)WeatherObservationMetrics.Barometric_Pressure]);
+
+            return new WeatherObservation()
+            {
+                TimeStamp = timestamp,
+                Barometric_Pressure = pressure
+            };
+        }
+
+        public static bool TryParse(string text, out WeatherObservation wo)
+        {
+            wo = new WeatherObservation() { Barometric_Pressure = float.NaN, TimeStamp = DateTime.MinValue };
+
+            var data = text.Split('\t');
+
+            if (data.Length != 8)
+                return false;
+
+            if (!DateTime.TryParse(ReplaceUnderScoreToDash(data[(int)WeatherObservationMetrics.Date_Time]), out DateTime dateTime))
+                return false;
+
+            if (!float.TryParse(ReplaceUnderScoreToDash(data[(int)WeatherObservationMetrics.Barometric_Pressure]), out float pressure))
+                return false;
+
+            wo = new WeatherObservation() { Barometric_Pressure = pressure, TimeStamp = dateTime };
+            return true;
+        }
+
+        private static string ReplaceUnderScoreToDash(string text) 
+        {
+            return text.Replace("_", "-");
+        }
+    }
+}
